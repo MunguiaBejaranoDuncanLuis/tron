c7e6222f6b0638521b313d441073414097b16d88
MunguiaBejaranoDuncanLuis <143463548+MunguiaBejaranoDuncanLuis@users.noreply.github.com>
2024-04-30 01:47:05 +0000

    Version 3

---
+++ src/pantalla.cpp
@@ -0,0 +1,29 @@
+#include <thread>
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <assets/images/moto.txt>
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full());
+
+    int x = 0;
+    while (true)
+    {
+        x++;
+        pantalla.Clear();
+        this_thread::sleep_for(0.1s);
+
+        pantalla.PixelAt(x,5).character = "Hola";
+        pantalla.PixelAt(x,6).character = moto.txt;
+
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
+    }
+
+    return 0;
+}
